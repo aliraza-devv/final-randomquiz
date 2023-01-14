7338c9eddb5dcd200a84ca945cbe109f6d19ad43
Ali Raza <aliraza.dev.web@gmail.com>
2023-01-15 03:19:38 +0500

    get data

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,35 +1,50 @@
 import React, { useState } from "react";
-// import { db } from "../firbase";
-import { useNavigate } from "react-router-dom";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 
 const Game = () => {
-  const navigate = useNavigate()
   const [num, setNum] = useState(0);
   const [score, setScore] = useState(0);
   const [isShown, setIsShown] = useState(false);
-  const [result, setResult] = useState([{
-    check: "",
-    answer: "",
-  }]);
-
-  // const handleUpload = () => {
-  //   db.ref('result/' + result.id).set({
-  //     result: result
-  //   })
-  // }
+  const [isShow, setIsShow] = useState(false);
+  const [result, setResult] = useState([
+    {
+      check: "",
+      answer: "",
+    },
+  ]);
 
   const writeResultData = () => {
     const db = getDatabase();
-    set(ref(db, 'result/' + result.id), {
-      result: result
+    set(ref(db, "result/" + result.id), {
+      result: result,
     });
-    navigate('/')
-  }
+  };
+
+  const getData = () => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `result/${result.id}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const displayData = () => {
+    setIsShow((current) => !current);
+  };
+
+  const handleRetry = () => {
+    setNum(0);
+  };
 
   const handleShow = () => {
     setIsShown((current) => !current);
-    console.log(result)
   };
 
   function randomNumberInRange(min, max) {
@@ -42,75 +57,101 @@ const Game = () => {
 
   const handleScore = () => {
     setScore(score + 1);
-    setResult([...result, {
-      check: "Right",
-      answer: num,
-    }]);
+    setResult([
+      ...result,
+      {
+        check: "Right",
+        answer: num,
+      },
+    ]);
   };
 
   const handleWrongScore = () => {
-    setResult([...result, {
-      check: "Wrong",
-      answer: num,
-    }]);
+    setResult([
+      ...result,
+      {
+        check: "Wrong",
+        answer: num,
+      },
+    ]);
   };
 
   return (
     <>
       <div className="game-container">
         <div className="game-container__inner">
-        <div>
-          <button onClick={handleClick} className="btn-secondary">
-            <span className="btn-secondary__span">Generate</span>
-          </button>
-        </div>
-        <div className="list-container">
-          <h3>Options are:</h3>
-          <ul className="lists">
-            <li onClick={handleWrongScore} className="list">
-            {num*1.7.toFixed()}
-            </li>
-            <li onClick={handleScore} className="list">
-              {num}
-            </li>
-            <li onClick={handleWrongScore} className="list">
-            {num*1.6.toFixed()}
-            </li>
-          </ul>
-          <div className="next-container">
-            <button className="btn-primary" onClick={handleClick}>
-              Next
-              <span className="arrow-wrapper">
-                <span className="arrow"></span>
-              </span>
+          <div>
+            <button onClick={handleClick} className="btn-secondary">
+              <span className="btn-secondary__span">Generate</span>
             </button>
           </div>
-        </div>
-        <div className="inner-container">
-          <div className="inner-container__box">
-            <button onClick={handleShow} className="btn-secondary">
-              <span className="btn-secondary__span">Result</span>
-            </button>
-            {isShown && (
-              <div>
-                <h2>{score} answers are correct</h2>
-                {/* <ul>
-                  {result.map((res) => (
-                    <li key={res.id}>{res.check}:  {res.answer}</li>
-                  ))}
-                </ul> */}
-                <div>
-                  <button onClick={writeResultData} className="btn-primary">
-                    Upload to Firebase
-                    <span className="arrow-wrapper">
-                      <span className="arrow"></span>
-                    </span>
-                  </button>
-                </div>
+          <div className="list-container">
+            <h3>Options are:</h3>
+            <ul className="lists">
+              <li onClick={handleWrongScore} className="list">
+                {num * (1.7).toFixed()}
+              </li>
+              <li onClick={handleScore} className="list">
+                {num}
+              </li>
+              <li onClick={handleWrongScore} className="list">
+                {num * (1.6).toFixed()}
+              </li>
+            </ul>
+            <div className="next-container">
+              <button className="btn-primary" onClick={handleClick}>
+                Next
+                <span className="arrow-wrapper">
+                  <span className="arrow"></span>
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className="inner-container">
+            <div className="inner-container__box">
+              <div className="d-flex">
+                <button onClick={handleShow} className="btn-secondary">
+                  <span className="btn-secondary__span">Result</span>
+                </button>
+                <button onClick={handleRetry} className="btn-secondary">
+                  <span className="btn-secondary__span">Retry</span>
+                </button>
               </div>
-            )}
+              {isShown && (
+                <div>
+                  <h2>{score} answers are correct</h2>
+                  <div className="d-flex">
+                    <div>
+                      <button onClick={writeResultData} className="btn-primary">
+                        Upload to Firebase
+                        <span className="arrow-wrapper">
+                          <span className="arrow"></span>
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+              <div className="marg">
+                <button onClick={displayData} className="btn-primary">
+                  Show Result
+                  <span className="arrow-wrapper">
+                    <span className="arrow"></span>
+                  </span>
+                </button>
+                {isShow && (
+                  <ul className="">
+                    <li className="list-result bold">R/W Answer</li>
+                    {result.map((res) => (
+                      <li className="list-result" key={res.id}>
+                        {res.check} {res.answer}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </>
---
+++ src/components/Result.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Result = () => {
+  return (
+    <div className="container">
+      <button className="btn-primary">
+        Show Data
+        <span className="arrow-wrapper">
+          <span className="arrow"></span>
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default Result;
